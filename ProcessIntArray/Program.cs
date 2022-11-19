c57100f8bdf7cfc3778653a99fe8fa0daa4871ce
Ryan Greenlee <ryan@rowanar.com>
2022-11-19 02:03:06 -0800

    Added new project C# console app ProcessIntArray.

---
+++ ProcessIntArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace ProcessIntArray
+{
+    internal class Program
+    {
+        static int findMedian(int[] nums) 
+        {
+            int median;
+            Array.Sort(nums);
+
+            // If we have an even number of samples, then take the average
+            if (nums.Length % 2 == 0)
+            {
+                int rightIndex = nums.Length / 2;
+                int leftIndex = rightIndex - 1;
+
+                median = (nums[leftIndex] + nums[rightIndex]) / 2;
+            }
+            else
+            {
+                median = nums[nums.Length / 2];
+            }
+
+            return median;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arrToSortEven = { 10, 99, 3, 19, 42, 17, 38, 6, 55, 21};
+            int[] arrToSortOdd = { 10, 99, 3, 19, 42, 17, 38, 6, 55, 21, 66};
+
+            int maxValue = arrToSortEven.Max();
+            int minValue = arrToSortEven.Min();
+            int medianEven = findMedian(arrToSortEven);
+            int medianOdd = findMedian(arrToSortOdd);
+
+        }
+    }
+}
---
+++ ProcessIntArray/README.md
@@ -0,0 +1,3 @@
+﻿From Reddit https://www.reddit.com/r/csharp/comments/yyz12e/stuck_on_net_sortarray_and_finding_median/
+
+Dropped some of the code because...I didn't want to deal with files
